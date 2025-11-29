d970cdeacaa1c284a40a9e71fb3dc991db7becd8
wugeqi <2466300758@qq.com>
2025-11-29 23:13:37 +0800

    补充建议2-多格式

--- src/main/java/org/autotestdemo/llm/LLMService.java
+++ src/main/java/org/autotestdemo/llm/LLMService.java
@@ -1,5 +1,7 @@
 package org.autotestdemo.llm;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public interface LLMService {
-    String getMessage(String query);
+    String getMessage(String query) throws JsonProcessingException;
 }
